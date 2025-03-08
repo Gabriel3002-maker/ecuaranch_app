1f5ee225b811a00065b592af425966057dcf1d58
Gabriel Octavio Vargas Vargas <gabriel.vargas@chvconsulting.es>
2025-03-08 15:36:34 -0500

    change styles in login and register

--- lib/views/auth/login_views.dart
+++ lib/views/auth/login_views.dart
@@ -1,39 +1,123 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            const TextField(
-              decoration: InputDecoration(labelText: 'Email'),
+      body: Stack(
+        children: [
+          // Fondo con la imagen (agrega tu propia imagen relacionada con ganadería)
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/campo_ganadero.png',  // Cambia a la imagen que prefieras
+              fit: BoxFit.cover,
             ),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Password'),
+          ),
+          // Fondo con un filtro oscuro para mejorar la visibilidad del formulario
+          Positioned.fill(
+            child: Container(
+              color: Colors.black.withOpacity(0.5), // Filtro oscuro
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Aquí va la lógica para hacer login (simplificada)
-                Navigator.pushReplacementNamed(context, '/dashboard');
-              },
-              child: const Text('Login'),
+          ),
+          // Formulario centrado sobre la imagen
+          Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/images/logoecuabyte.png', 
+                    height: 120,
+                  ),
+                  const SizedBox(height: 40),
+                  // Campo de texto para Email
+                  const TextField(
+                    style: TextStyle(color: Colors.white), // Color de texto blanco
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(color: Colors.white), // Color de la etiqueta blanco
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF6B8E23), width: 2),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFBDB76B), width: 2), // Beige
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  // Campo de texto para Password
+                  TextField(
+                    style: const TextStyle(color: Colors.white), // Color de texto blanco
+                    obscureText: _obscureText,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: const TextStyle(color: Colors.white), // Color de la etiqueta blanco
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF6B8E23), width: 2),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFBDB76B), width: 2), // Beige
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _obscureText ? Icons.visibility : Icons.visibility_off,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _obscureText = !_obscureText;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  // Botón de Login
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: const Color(0xFF6B8E23), backgroundColor: Colors.white, // Texto verde
+                      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 32),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/dashboard');
+                    },
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(fontSize: 18, color: Color(0xFF6B8E23)), // Texto verde
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  // Botón de registro
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/register');
+                    },
+                    child: const Text(
+                      'Don\'t have an account? Register',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 55, 147, 205), // Verde oliva
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/register');
-              },
-              child: const Text('Don\'t have an account? Register'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/views/auth/register_views.dart
+++ lib/views/auth/register_views.dart
@@ -1,43 +1,151 @@
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
+
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  bool _obscureTextPassword = true;
+  bool _obscureTextConfirmPassword = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const  Text('Register')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            const TextField(
-              decoration: InputDecoration(labelText: 'Email'),
+      body: Stack(
+        children: [
+          // Fondo con la imagen (agrega tu propia imagen relacionada con ganadería)
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/campo_ganadero.png', // Cambia a la imagen que prefieras
+              fit: BoxFit.cover,
             ),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Password'),
+          ),
+          // Fondo con un filtro oscuro para mejorar la visibilidad del formulario
+          Positioned.fill(
+            child: Container(
+              color: Colors.black.withOpacity(0.5), // Filtro oscuro
             ),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Confirm Password'),
+          ),
+          // Formulario centrado sobre la imagen
+          Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/images/logoecuabyte.png', // Logo de la empresa ganadera
+                    height: 120,
+                  ),
+                  const SizedBox(height: 40),
+                  // Campo de texto para Email
+                  const TextField(
+                    style: TextStyle(color: Colors.white), // Color de texto blanco
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(color: Colors.white), // Color de la etiqueta blanco
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF6B8E23), width: 2),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFBDB76B), width: 2), // Beige
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  // Campo de texto para Password
+                  TextField(
+                    style: const TextStyle(color: Colors.white), // Color de texto blanco
+                    obscureText: _obscureTextPassword,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: const TextStyle(color: Colors.white), // Color de la etiqueta blanco
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF6B8E23), width: 2),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFBDB76B), width: 2), // Beige
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _obscureTextPassword ? Icons.visibility : Icons.visibility_off,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _obscureTextPassword = !_obscureTextPassword;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  // Campo de texto para Confirm Password
+                  TextField(
+                    style: const TextStyle(color: Colors.white), // Color de texto blanco
+                    obscureText: _obscureTextConfirmPassword,
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      labelStyle: const TextStyle(color: Colors.white), // Color de la etiqueta blanco
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF6B8E23), width: 2),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFBDB76B), width: 2), // Beige
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _obscureTextConfirmPassword ? Icons.visibility : Icons.visibility_off,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _obscureTextConfirmPassword = !_obscureTextConfirmPassword;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  // Botón de Register
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: const Color(0xFF6B8E23), backgroundColor: Colors.white, // Texto verde
+                      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 32),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/dashboard');
+                    },
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(fontSize: 18, color: Color(0xFF6B8E23)), // Texto verde
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  // Botón para ir a Login
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/');
+                    },
+                    child: const Text(
+                      'Already have an account? Login',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 55, 147, 205), // Verde oliva
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Aquí va la lógica para el registro (simplificada)
-                Navigator.pushReplacementNamed(context, '/dashboard');
-              },
-              child: const Text('Register'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/');
-              },
-              child: const  Text('Already have an account? Login'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
